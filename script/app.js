fcd84649190969bbbb6e869e757411703981de5f
kashifali554 <kashifali874@gmail.com>
2017-03-03 19:21:43 -0800

    I have changed the whole game logic, I was thinking to GET the col-md-1 as an array and use for or forEach loop to itriate through the col-md-1 array but it gives back the object. So, I am trying to create animation logic. Just only one row, and GETing player1 class via jquery and moving player1 div class. Next, Define the wining function and countdown function.

--- script/app.js
+++ script/app.js
@@ -1,30 +1,72 @@
-console.log("Sanity Check");
-
 $(document).ready(function () {
 	console.log("Document ready is working");
 
-	var boxes = $(".col-md-1");
+
+	moveRight();
+
+	function countDown(){
+		
+	}
+
+	function moveRight(){
+		$( "body" ).keydown(function( event ) {
+			event.preventDefault();
+		if (event.which === 75 || event.which === 107) {
+			$(".player1").animate({left: "+=20px"}, 0)
+			}
+		});
+
+		$( "body" ).keydown(function( event ) {
+			event.preventDefault();
+		if (event.which === 83 || event.which === 115 ) {
+		$(".player2").animate({left: "+=20px"}, 0)
+			}
+		});
+	}
+	// $('.playerOne').on('click', function(){
+	// 	$('.playerOne').animate({right: "+=20px"}, 0)
+
+	// })
+
+	// $("body").animate({left: "+=50px"}, 0);
+
+	// $('#playerOne').keydown(function(event) {
+	//   if ( event.which === 83 || event.which === 115 ) {
+	//      event.preventDefault();
+	//      $('#playerOne').animate({
+	//      	right: "+=50px"
+	//      }, 0);
+	//   }
+	// });
 
 
-	$("#trackOne").keypress(function(event) {
-	  if (event.which === 75 || event.which === 107) {
-	     event.preventDefault();
-	     console.log("Player One Key Pressed");
-	  }
-	});
+	// var PlayerOne = $('img');
+	// // var boxes = $(".col-md-1");
 
-	$("body").keypress(function(event) {
-	  if (event.which === 83 || event.which === 115 ) {
-	     event.preventDefault();
-	     console.log("Player Two Key Pressed");
-	  }
-	});
 
-	// box.on("keypress.75", function handleClick(){
-	// 	box.append("This rocket is moving");
+	// $("body").keypress(function(event) {
+	//   if (event.which === 75 || event.which === 107) {
+	//      event.preventDefault();
+	//      console.log("Player One Key pressed");
+	//     playerOne.animate({left: "+=50px"}, 0);
+	//   }
+	// });
 
-	
-	//  });
- 	
+	// $("body").keypress(function(event) {
+	//   if (event.which === 83 || event.which === 115 ) {
+	//      event.preventDefault();
+	//      console.log("Player Two Key Pressed");
+	//      $("#trackTwo")animate({left: "+=50px"}, 0);
+	//   }
+	// });
+
+	// 	boxes.on("click", function handleClick(){
+	//	boxes.append("This rocket is moving");
+	// 	for (i = 0; i < boxes.length; i++) { 
+	//	console.log(boxes[i]);
+	// }
+
+	// });
+
  });
 
